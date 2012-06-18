f5941d1b92303aa646677b23a69538ce6202a5fd
Paul Coyle <paul@paulcoyle.com>
2012-06-18 11:47:16 -0600

    Avoid overwriting files who faile gofmt.

--- tools/fmt.sh
+++ tools/fmt.sh
@@ -1,7 +1,6 @@
 TEMPFILE="__gofmt.temp"
 for f in `find . -name "*.go"`; do
   echo "Formatting $f..."
-  gofmt -tabs=false -tabwidth=2 $f > $TEMPFILE
-  mv $TEMPFILE $f
+  gofmt -tabs=false -tabwidth=2 $f > $TEMPFILE && mv $TEMPFILE $f
 done
 echo "Done!"
